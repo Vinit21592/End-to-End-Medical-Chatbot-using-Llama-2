d2f7bf6048a9b1cf5bc5e4d6d785523b124d0dc1
vinit <vinit.londhe21@gmail.com>
2023-12-20 19:14:44 +0530

    app.py added

--- template.py
+++ template.py
@@ -33,4 +33,4 @@ for filepath in list_of_files:
             logging.info(f"Creating empty file: {filepath}")
  
     else:
-        logging.info(f"{filename} is already exists")
+        logging.info(f"{filename} already exists")
